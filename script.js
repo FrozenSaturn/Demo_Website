4729e3389adf305f337e2b01029ee578f7381935
Kaushik Samadder <70917815+Kaushik2003@users.noreply.github.com>
2024-09-08 10:11:22 +0530

    nav bar done

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-// Main JS
+
+"use strict";
+
+var reply_click = function()
+{
+    alert("Button clicked, id "+this.id+", text"+this.innerHTML);
+}
+
+function redirectToPage(page) {
+    window.location.href = page;
+}
+document.getElementById('home').onclick = ()=>{
+    redirectToPage('./index.html');
+};
+
+document.getElementById('about').onclick = ()=>{
+    redirectToPage('./about.html');
+};
+
+document.getElementById('contact').onclick = ()=>{
+    
+    redirectToPage('./contact.html');
+};
+
+document.getElementById('forum').onclick = ()=>{
+   
+    redirectToPage('./forum.html');
+};
+
+document.getElementById('news').onclick = ()=>{
+    
+    redirectToPage('./news.html');
+};
+
+
